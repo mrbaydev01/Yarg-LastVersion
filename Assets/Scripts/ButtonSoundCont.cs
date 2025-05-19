fc77add016e4cd04487ac9edb538253a4b92d288
mrbaydev01 <emre2002bayrak81@gmail.com>
2025-05-19 12:32:52 +0300

    MENÜ Arkaplanı eklendi

--- Assets/Scripts/ButtonSoundCont.cs
+++ Assets/Scripts/ButtonSoundCont.cs
@@ -5,15 +5,27 @@ using UnityEngine.UI;
 
 public class ButtonSoundCont : MonoBehaviour
 {
- public AudioSource audioSource;
+    public AudioSource audioSource;
     public AudioClip buttonClickSound;
-   public void PlaySound()
+
+    [Header("Müzik Ayarları")]
+    public bool ileriBaşlat = true;
+    public float baslangicSuresi = 10f;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+
+        if (ileriBaşlat && audioSource != null && audioSource.clip != null)
+        {
+            baslangicSuresi = Mathf.Clamp(baslangicSuresi, 0, audioSource.clip.length - 0.1f);
+            audioSource.time = baslangicSuresi;
+            audioSource.Play();
+        }
+    }
+
+    public void PlaySound()
     {
         audioSource.PlayOneShot(buttonClickSound);
     }
-    void Awake()
-{
-    DontDestroyOnLoad(this.gameObject);
 }
-}
-
